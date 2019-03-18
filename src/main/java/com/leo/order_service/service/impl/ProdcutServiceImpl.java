c05d792150501af6890425ed0ef95d8878424363
Leo Liu <scau_lzc@163.com>
2019-03-18 21:39:11 +0800

    change api to spring cloud openfeign

--- src/main/java/com/leo/order_service/service/impl/ProdcutServiceImpl.java
+++ src/main/java/com/leo/order_service/service/impl/ProdcutServiceImpl.java
@@ -1,14 +1,18 @@
 package com.leo.order_service.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
 import com.leo.order_service.model.ProductOrder;
+import com.leo.order_service.service.ProductClient;
 import com.leo.order_service.service.ProductService;
+import com.leo.order_service.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -17,16 +21,20 @@ public class ProdcutServiceImpl implements ProductService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private ProductClient productClient;
+
     @Override
     public ProductOrder save(Integer userId, Integer productId) {
-        Map map = restTemplate.getForObject("http://product-service/api/v1/product/find?id=" + productId, Map.class);
-        Preconditions.checkNotNull(map);
+        String productJson = productClient.findById(productId);
+        JsonNode jsonNode = JsonUtils.str2JsonNode(productJson);
+        Preconditions.checkNotNull(jsonNode);
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCreateTime(LocalDateTime.now());
         productOrder.setUserId(userId);
         productOrder.setTradeNo(UUID.randomUUID().toString());
-        productOrder.setProductName(map.get("name").toString());
-        productOrder.setPrice(map.get("price").toString());
+        productOrder.setProductName(jsonNode.get("name").toString());
+        productOrder.setPrice(jsonNode.get("price").toString());
         return productOrder;
     }
 }

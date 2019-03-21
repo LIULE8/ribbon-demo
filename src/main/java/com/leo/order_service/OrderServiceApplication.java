e229229b466c4aae16d6b9ef689021f2a7b23367
leo liu <scau_lzc@163.com>
2019-03-21 21:55:32 +0800

    add hystrix code

--- src/main/java/com/leo/order_service/OrderServiceApplication.java
+++ src/main/java/com/leo/order_service/OrderServiceApplication.java
@@ -3,6 +3,8 @@ package com.leo.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
+//@SpringCloudApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/leo/order_service/controller/ProductController.java
+++ src/main/java/com/leo/order_service/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.leo.order_service.controller;
 
+import com.google.common.collect.Maps;
 import com.leo.order_service.service.ProductService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -12,8 +17,25 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("save")
+    @HystrixCommand(fallbackMethod = "saveFailCallBack")
     public Object save(@RequestParam("user_id") Integer userId,
                        @RequestParam("product_id") Integer productId) {
-        return productService.save(userId, productId);
+        Map<String, Object> msg = Maps.newHashMap();
+        msg.put("code",-1);
+        msg.put("data",productService.save(userId, productId));
+        return msg;
+    }
+
+    /**
+     * 方法签名需要和api方法一致
+     * @param userId
+     * @param productId
+     * @return
+     */
+    public Object saveFailCallBack(Integer userId,Integer productId){
+        Map<String, Object> msg = Maps.newHashMap();
+        msg.put("code",-1);
+        msg.put("msg","抢购人数太多，请稍后重试");
+        return msg;
     }
 }
